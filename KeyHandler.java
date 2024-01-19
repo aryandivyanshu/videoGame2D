c12d91495b73fc13eea684857a8f4db8f22f18c4
Aryan Divyanshu <52078579+aryandivyanshu@users.noreply.github.com>
2024-01-19 09:18:52 +0530

    Update KeyHandler.java

--- KeyHandler.java
+++ KeyHandler.java
@@ -12,14 +12,15 @@ public class KeyHandler implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        //this method sets value of variables like upPressed, leftPressed, etc. to 'true' when particular keys are pressed 
+        
         int code = e.getKeyCode();
         /*
         https://stackoverflow.com/questions/15313469/java-keyboard-keycodes-list
          */
 
         if (code == KeyEvent.VK_W){
-            upPressed = true;
+            upPressed = true; 
         }
 
         if (code == KeyEvent.VK_A){
@@ -37,6 +38,7 @@ public class KeyHandler implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+        //this method sets value of variables like upPressed, leftPressed, etc. to 'false' when particular keys are pressed 
 
         int code = e.getKeyCode();
 
